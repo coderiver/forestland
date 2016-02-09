a270ca2f63c779f7d3086db52d70b7afa46b4d15
Ex-Zy <serega1905@ukr.net>
2016-02-09 17:34:56 +0200

    add nav mobile

--- src/js/common.js
+++ src/js/common.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
 		autoplaySpeed: 8000,
 		slidesToShow: 1,
 	});
+	$(window).on('scroll', function() {
+		if($(window).width() > 1365) {
+			if($(window).scrollTop() > 0) {
+				$(".header__logo").removeClass('header__logo_big');
+			} else {
+				$(".header__logo").addClass('header__logo_big');
+			}
+		}
+	});
+	$('.js-humb').on('click', function() {
+		$('.header__nav-mob').toggleClass('is-visible');
+	});
 });
